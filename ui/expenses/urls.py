b5b3671e1eb7ffdb2d50740049c55aee290c055a
Andriy Artamonov <andreiartamonovaa@gmail.com>
2021-11-12 16:30:03 +0200

    UI added base.html

--- ui/expenses/urls.py
+++ ui/expenses/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('charts', views.user_expenses, name='user_expenses'),
-    path('add_expenses', views.add_expenses, name='add_expenses'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
+    path('charts/', views.user_expenses, name='user_expenses'),
+    path('add_expenses/', views.add_expenses, name='add_expenses'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
